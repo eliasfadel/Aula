dccec8dc64f8df3806d145b6d14cce7ddb90cbf5
eliasfadel <eliasfadel@usp.br>
2020-05-06 13:12:26 -0300

    aula_6

---
+++ aula_6/Aula_6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    float x;
+
+    a = 2;
+    b = 3;
+
+    x = 6.2;
+
+    printf ("a vale %d, b vale %d, x vale %d\n", a, b, x);
+
+
+    return 0;
+
+
+
+}
